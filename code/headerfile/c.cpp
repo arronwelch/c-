bfd32adaf2287dc9b654bdc6d7870d9c723cdc0b
deft_ubuntu <gostnq@gmail.com>
2022-01-16 02:18:08 +0800

    review

--- code/headerfile/c.cpp
+++ code/headerfile/c.cpp
@@ -1,34 +1,57 @@
+#include <stdio.h>
 
-#include <iostream>
-using namespace std;
-
-// extern int gl;
+//#include <iostream>
+//using namespace std;
 
 class A
 {
-    // private:
-    //     int i;
 public:
+    A(int initialize);
+    ~A();
     int i;
     void f();
+};
+
+struct B
+{
+    int i;
 };
 
 void A::f()
 {
     i = 20;
-    cout << i << endl;
+    printf("&i = %p\n",&i);
+}
+
+void A::A( int initialize)
+{
+    i = initialize;
+    printf("In init\n");
+}
+
+void A::~A()
+{
+    printf("In de\n");
+}  
+
+void f(struct B* p)
+{
+    p->i = 21;
 }
 
 int main()
 {
-    A a;
-    A b;
-    a.i = 10;
-    cout << a.i << endl;
+    A a(10);
+    B b;
+    //cout << a.i << endl;
+    printf("&a = %p\n",&a);
+    printf("&a.i=%p\n",&a.i);
     a.f();
-    cout << a.i << endl;
+    //cout << a.i << endl;
 
-    cout << "-------" << endl;
-    b.f();
-    cout << b.i << endl;
+    //cout << "-------" << endl;
+    b.i = 11;
+    //cout << b.i << endl;
+    f(&b);
+    //cout << b.i << endl;
 }
---
+++ code/headerfile/c.s
@@ -0,0 +1,126 @@
+	.file	"c.cpp"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"&i = %p\n"
+	.text
+	.align 2
+	.globl	_ZN1A1fEv
+	.type	_ZN1A1fEv, @function
+_ZN1A1fEv:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	$20, 4(%rax)
+	movq	-8(%rbp), %rax
+	addq	$4, %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_ZN1A1fEv, .-_ZN1A1fEv
+	.globl	_Z1fP1B
+	.type	_Z1fP1B, @function
+_Z1fP1B:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	$21, (%rax)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	_Z1fP1B, .-_Z1fP1B
+	.section	.rodata
+.LC1:
+	.string	"&a = %p\n"
+.LC2:
+	.string	"&a.i=%p\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$10, -12(%rbp)
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	addq	$4, %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZN1A1fEv
+	movl	$11, -20(%rbp)
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rdi
+	call	_Z1fP1B
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
