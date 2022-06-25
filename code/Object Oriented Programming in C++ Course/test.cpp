e72973ffd6c980b808812e7c04a48ed6c613eff1
deft <Davego@protonmail.com>
2022-06-26 00:02:49 +0800

    abstract

--- code/Object Oriented Programming in C++ Course/test.cpp
+++ code/Object Oriented Programming in C++ Course/test.cpp
@@ -3,43 +3,74 @@
 
 using std::string;
 
-class Employee{
-//private:// defualt
-public:
-//protected:
+class AbstractEmployee {
+	virtual void AskForPromotion()=0;
+};
+
+class Employee:AbstractEmployee{
+private:// defualt
 	string Name;
 	string Company;
 	int Age;
 
+public:
+	void setName(string name)
+	{
+		Name = name;
+	}
+	string getName(void)
+	{
+		return Name;
+	}
+	void setCompany(string company)
+	{
+		Company = company;
+	}
+	string getCompany(void)
+	{
+		return Company;
+	}
+	void setAge(int age)
+	{
+		if ( age >= 18 )
+		{
+			Age = age;
+		}
+	}
+	int getAge(void)
+	{
+		return Age;
+	}
+	
 	void IntroduceYourself() {
 		std::cout << "Name - " << Name << std::endl;
 		std::cout << "Company - " << Company << std::endl;
-		std::cout << "Age - " << Age << std::endl;
+		std::cout << "Age - " << Age << "\n" << std::endl;
 	}
 	Employee(string name, string company, int age){
 		Name = name;
 		Company = company;
 		Age = age;
 	}
+	void AskForPromotion(void){
+		if(Age > 30)
+		{
+			std::cout << Name << " got promoted!" << std::endl;
+		}
+		else
+		{
+			std::cout << Name << ", sorry No promotion for you!" << std::endl;
+		}
+	}
+protected:
 };
 
 int main(void)
 {
-	std::cout << "Hello, World!" << std::endl;
-
-	//Employee employee1;
-	//employee1.Name = "Saldina";
-	//employee1.Company = "YT-CodeBeauty";
-	//employee1.Age = 25;
 	Employee employee1 = Employee("Saldina", "YT-CodeBeaty", 25);
-	employee1.IntroduceYourself();
-
-	//Employee employee2;
-	//employee2.Name = "John";
-	//employee2.Company = "Amazon";
-	//employee2.Age = 35;
 	Employee employee2 = Employee("John", "Amazon", 35);
-	employee2.IntroduceYourself();
+	employee1.AskForPromotion();
+	employee2.AskForPromotion();
 
 	return 0;
 }
