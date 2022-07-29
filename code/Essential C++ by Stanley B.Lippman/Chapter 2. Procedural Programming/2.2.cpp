ba9a18a7ef89ddbe6bb66698bc6fca0c9d6ebdf0
deft <Davego@protonmail.com>
2022-07-29 19:09:01 +0800

    reference in bubble_sort

--- code/Essential C++ by Stanley B.Lippman/Chapter 2. Procedural Programming/2.2.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 2. Procedural Programming/2.2.cpp
@@ -13,17 +13,34 @@ void display(vector<int> vec)
 	cout << endl;
 }
 
-ofstream ofil("text_out1");
-void swap(int val1, int val2)
-{
-	ofil << "swap(" << val1
-		 << ", " << val2 << ")\n";
+/*
+ * OK: by declaring val1 and val2 as references
+ *     changes to the two parameters within swap()
+ *     are reflected in the objects passed to swap()
+ */
 
+// ofstream ofil("text_out1");
+void swap(int & val1, int & val2)
+{
+	// cout << "swap(" << val1
+	// 	 << ", " << val2 << ")\n";
+
+	// what is the relationship between
+	// the formal parameters val1, val2
+
+	// and the actual arguments vec[ix] and vec[jx]?
+
+	/*
+	 * note that our code within swap()
+	 * does not change -- only the relationship
+	 * between the parameters of swap() and the
+	 * objects passed to swap() changes
+	 */
 	int temp = val1;
 	val1 = val2;
 	val2 = temp;
-	ofil << "after swap(): val1 " << val1
-		 << " val2:" << val2 << "\n";
+	// cout << "after swap(): val1 " << val1
+	// 	 << " val2:" << val2 << "\n";
 }
 
 // void swap(int &val1, int &val2)
@@ -34,15 +51,16 @@ void swap(int val1, int val2)
 // }
 
 // ofstream ofil("text_out1");
-void bubble_sort(vector<int> vec)
+void bubble_sort(vector<int> &vec)
 {
 	for(int ix = 0; ix < vec.size(); ++ix)
 		for (int jx = ix + 1; jx < vec.size(); ++jx)
 			if(vec[ix] > vec[jx]) {
-				ofil << "about to call swap!"
-					 << " ix: " << ix << " jx: " << jx << '\t'
-					 << " swapping: " << vec[ix]
-					 << " with " << vec[jx] << endl;
+				// cout << "about to call swap!"
+				// 	 << " ix: " << ix << " jx: " << jx << '\t'
+				// 	 << " swapping: " << vec[ix]
+				// 	 << " with " << vec[jx] << endl;
+				// display(vec);
 
 					// ok: actual swap code ...
 				swap(vec[ix], vec[jx]);
