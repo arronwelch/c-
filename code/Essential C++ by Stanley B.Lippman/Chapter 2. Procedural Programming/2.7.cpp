ebd18c520270a1f7bfa499d1f0c06be9827a3af4
deft <Davego@protonmail.com>
2022-08-02 02:06:07 +0800

    template function

--- code/Essential C++ by Stanley B.Lippman/Chapter 2. Procedural Programming/2.7.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 2. Procedural Programming/2.7.cpp
@@ -1,11 +1,17 @@
 // 2.7 Defining and Using Template Functions
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 void display_message(const string&, const vector<int>&);
 void display_message(const string&, const vector<double>&);
 void display_message(const string&, const vector<string>&);
 
 void display_message(const string&, const vector<string>&,
 					 ostream& = cout);
+
 void display_message(const string &msg, const vector<int> &vec)
 {
 	cout << msg;
@@ -22,3 +28,21 @@ void display_message(const string &msg, const vector<string> &vec)
 	cout << '\n';
 }
 
+template <typename elemType>
+void display_message(const string &msg,
+					 const vector<elemType> &vec)
+{
+	cout << msg;
+	for (int ix = 0; ix < vec.size(); ++ix)
+	{
+		elemType t = vec[ix];
+		cout << t << ' ';
+	}
+}
+
+int main(void)
+{
+	cout << "Hello!\n";
+
+	return 0;
+}
