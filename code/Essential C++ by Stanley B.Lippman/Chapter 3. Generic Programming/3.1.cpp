f2b932da3260b191c520059697bde486f7f44262
deft <Davego@protonmail.com>
2022-08-11 01:26:20 +0800

    generic array find function

--- code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.cpp
@@ -49,5 +49,44 @@ array[2];
 
 }
 
+template <typename> elemType>
+elemType* find(const elemType *array, int size,
+				const elemType &value)
+{
+	if (! array || size < 1) return 0;
 
+	// ++array increments array by one element
+	for (int ix = 0; ix < size; ++ix, ++array)
+		// *array dereferences the address
+		if (*array == value)
+			return array;
+	return 0;
+}
+
+template <typename elemType>
+elemType* find(const elemType *first,
+				const elemType *last, const elemType &value)
+{
+	if (! first || ! last)
+		return 0;
+
+	// while first does not equal to last,
+	// compare value with element addressed by first
+	// if the two are equal, return first
+	// otherwise, increment first to address next element
+
+	for(; first != last; ++first)
+		if (*first == value)
+			return first;
+
+	return first;
+}
+
+int			ia[8] = { 1, 1, 2, 3, 5, 8, 13, 21 };
+double		da[6] = { 1.5, 2.0, 2.5, 3.0, 3.5, 4.0 };
+string		sa[4] = { "pooh", "piglet", "eeyore", "tigger" };
+
+int			*pi = find(ia, ia+8, ia[3]);
+double		*pd = find(da, da+6, da[3]);
+string		*ps = find(sa, sa+4, sa[3]);
 
