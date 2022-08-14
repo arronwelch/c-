4bace7ed5919b4455debf0fec8509a16517ac203
deft <Davego@protonmail.com>
2022-08-15 01:45:07 +0800

    array and vector in a instance of find

--- code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.cpp
@@ -1,70 +1,75 @@
 // 3.1 The Arithmetic of Pointers
 
-int* find(const vector<int> &vec, int value)
-{
-	for(int ix = 0; ix < vec.size(); ++ix)
-		if (vec[ix] == value)
-			return &vec[ix];
-	return 0;
-}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// int* find(const vector<int> &vec, int value)
+// {
+// 	for(int ix = 0; ix < vec.size(); ++ix)
+// 		if (vec[ix] == value)
+// 			return &vec[ix];
+// 	return 0;
+// }
+// 
+// template <typename elemType>
+// elemType* find(const vector<elemType> &vec,
+// 				const elemType &value)
+// {
+// 	for (int ix = 0; ix < vec.size(); ++ix)
+// 		if (vec[ix] == value)
+// 			return &vec[ix];
+// 	return 0;
+// }
+// 
+// template <typename elemType>
+// elemType* find(const elemType *array, int size,
+// 				const elemType &value);
+// 
+// template <typename elemType>
+// elemType* find(const elemType *first, const elemType *sentinel,
+// 				const elemType &value);
+// 
+// template <typename elemType>
+// elemType* find(const elemType *array, int size,
+// 				const elemType &value)
+// {
+// 	if (! array || size < 1)
+// 		return 0;
+// 
+// 	for (int ix = 0; ix < size; ++ix)
+// 		// we can apply subscript operator to pointer
+// 		if (array[ix] == value)
+// 			return &array[ix];
+// 
+// 	return 0; // value not found
+// 
+// array[2];
+// 
+// // returns the value of the third element of the array(indexing, remenber, begins at 0). The
+// // following also returns the value of the third element:
+// 
+// *(array + 2);
+// 
+// }
+// 
+// template <typename> elemType>
+// elemType* find(const elemType *array, int size,
+// 				const elemType &value)
+// {
+// 	if (! array || size < 1) return 0;
+// 
+// 	// ++array increments array by one element
+// 	for (int ix = 0; ix < size; ++ix, ++array)
+// 		// *array dereferences the address
+// 		if (*array == value)
+// 			return array;
+// 	return 0;
+// }
 
 template <typename elemType>
-elemType* find(const vector<elemType> &vec,
-				const elemType &value)
-{
-	for (int ix = 0; ix < vec.size(); ++ix)
-		if (vec[ix] == value)
-			return &vec[ix];
-	return 0;
-}
-
-template <typename elemType>
-elemType* find(const elemType *array, int size,
-				const elemType &value);
-
-template <typename elemType>
-elemType* find(const elemType *first, const elemType *sentinel,
-				const elemType &value);
-
-template <typename elemType>
-elemType* find(const elemType *array, int size,
-				const elemType &value)
-{
-	if (! array || size < 1)
-		return 0;
-
-	for (int ix = 0; ix < size; ++ix)
-		// we can apply subscript operator to pointer
-		if (array[ix] == value)
-			return &array[ix];
-
-	return 0; // value not found
-
-array[2];
-
-// returns the value of the third element of the array(indexing, remenber, begins at 0). The
-// following also returns the value of the third element:
-
-*(array + 2);
-
-}
-
-template <typename> elemType>
-elemType* find(const elemType *array, int size,
-				const elemType &value)
-{
-	if (! array || size < 1) return 0;
-
-	// ++array increments array by one element
-	for (int ix = 0; ix < size; ++ix, ++array)
-		// *array dereferences the address
-		if (*array == value)
-			return array;
-	return 0;
-}
-
-template <typename elemType>
-elemType* find(const elemType *first,
+elemType* my_find(elemType *first,
 				const elemType *last, const elemType &value)
 {
 	if (! first || ! last)
@@ -79,19 +84,40 @@ elemType* find(const elemType *first,
 		if (*first == value)
 			return first;
 
-	return first;
+	return 0;
 }
 
-int			ia[8] = { 1, 1, 2, 3, 5, 8, 13, 21 };
-double		da[6] = { 1.5, 2.0, 2.5, 3.0, 3.5, 4.0 };
-string		sa[4] = { "pooh", "piglet", "eeyore", "tigger" };
 
-int			*pi = find(ia, ia+8, ia[3]);
-double		*pd = find(da, da+6, da[3]);
-string		*ps = find(sa, sa+4, sa[3]);
+/* A vector also holds its elements in a contiguous area of memory, so we can pass find()
+ * a begin/end pair of addresses in the same way as we do for the built-in array, except in
+ * one case.Unlike an array,a vector can be empty.For example,
+ */
 
-vector<string> svec;
+// vector<string> svec;
+// 
+// find(&svec[0], &svec[svec.size()], search_value);
+// 
+// if (! svec.empty()) // ... ok, call find()
 
-find(&svec[0], &svec[svec.size()], search_value);
+template <typename elemType>
+inline elemType* begin(const vector<elemType> &vec)
+	{ return vec.empty() ? 0 : &vec[0]; }
 
-if (! svec.empty()) // ... ok, call find()
+// find(begin(svec), end(svec), search_value);
+
+int main(void)
+{
+	int			ia[8] = { 1, 1, 2, 3, 5, 8, 13, 21 };
+	double		da[6] = { 1.5, 2.0, 2.5, 3.1, 3.5, 4.0 };
+	string		sa[4] = { "pooh", "piglet", "eeyore", "tigger" };
+	
+	int			*pi = my_find(ia, ia+8, ia[3]);
+	double		*pd = my_find(da, da+6, da[3]);
+	string		*ps = my_find(sa, sa+4, sa[3]);
+
+	cout << "*pi = " << *pi << endl;
+	cout << "*pd = " << *pd << endl;
+	cout << "*ps = " << *ps << endl;
+
+	return 0;
+}
