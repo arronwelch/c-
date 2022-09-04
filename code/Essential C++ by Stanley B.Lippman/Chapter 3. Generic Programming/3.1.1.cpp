db1390ae8baf868c0947c726242cb4c9f71c03d8
deft <Davego@protonmail.com>
2022-09-04 23:45:29 +0800

    new review generic programming

---
+++ code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.1.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int* my_find(const vector<int> &vec, int &value)
+{
+	for (int ix = 0; ix < vec.size(); ++ix)
+		if (vec[ix] == value)
+			return &vec[ix];
+
+	return 0;
+}
+
+int main(void)
+{
+	int ia[5] = { 42, 35, 12, 73, 19 };
+	vector<int> ivec(ia, ia+5);
+	int value = 0;
+	const int* find_value = NULL;
+
+	for (int ix = 0; ix < 5; ++ix)
+		cout << ia[ix] << ' ';
+	cout << endl;
+
+	value = 12;
+	find_value = my_find(ivec, value);
+	if (find_value != 0)
+		cout << "find the value " << *find_value << endl;
+	else
+		cout << "not find value " << value << endl;
+
+	value = 13;
+	find_value = my_find(ivec, value);
+	if (find_value != 0)
+		cout << "find the value " << *find_value << endl;
+	else
+		cout << "not find value " << value << endl;
+
+	return 0;
+}
---
+++ code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.2.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// int* my_find(vector<int> &vec, int value)
+// {
+// 	for (int ix = 0; ix < vec.size(); ++ix)
+// 		if (vec[ix] == value)
+// 			return &vec[ix];
+// 
+// 	return 0;
+// }
+
+template <typename elemType>
+const elemType* my_find(const vector<elemType> &vec,
+						const elemType &value)
+{
+	for (int ix = 0; ix < vec.size(); ++ix)
+		if (vec[ix] == value)
+			return &vec[ix];
+
+	return 0;
+}
+
+int main(void)
+{
+	int ia[5] = { 42, 35, 12, 73, 19 };
+	vector<int> ivec(ia, ia+5);
+	int value = 0;
+	const int* find_value = NULL;
+
+	for (int ix = 0; ix < 5; ++ix)
+		cout << ia[ix] << ' ';
+	cout << endl;
+
+	value = 12;
+	find_value = my_find(ivec, value);
+	if (find_value != 0)
+		cout << "find the value " << *find_value << endl;
+	else
+		cout << "not find value " << value << endl;
+
+	value = 13;
+	find_value = my_find(ivec, value);
+	if (find_value != 0)
+		cout << "find the value " << *find_value << endl;
+	else
+		cout << "not find value " << value << endl;
+
+	return 0;
+}
