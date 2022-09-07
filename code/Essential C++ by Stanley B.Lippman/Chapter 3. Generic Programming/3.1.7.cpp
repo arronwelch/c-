3cf1756ecc0249368204079179df387eaf84e97e
deft <Davego@protonmail.com>
2022-09-08 00:54:08 +0800

    finish find() with pointer and template

--- code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.7.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 3. Generic Programming/3.1.7.cpp
@@ -7,19 +7,26 @@ using namespace std;
 
 template <typename elemType>
 const elemType* my_find(const elemType* first,
-						cosnt elemType* second,
+						const elemType* last,
 						const elemType &value)
 {
-	if (! 
+	if (! first || ! last)
+		return 0;
+
+	for(; first != last; ++first)
+		if(*first == value)
+			return first;
+
+	return 0;
 }
 
 template <typename elemType>
-inline elemType* begin(const vector<elemType> &vec)
-	{ return vec.empty() ? 0 : &vec[0]; }
+inline const elemType* my_begin(const vector<elemType> &vec)
+	{ return (vec.empty() ? 0 : &vec[0]); }
 
 template <typename elemType>
-inline elemType* end(const vector<elemType> &vec)
-	{ return vec.empty() ? 0 : &vec[vec.size()] }
+inline const elemType* my_end(const vector<elemType> &vec)
+	{ return (vec.empty() ? 0 : &vec[vec.size()]); }
 
 int main(void)
 {
@@ -34,11 +41,21 @@ int main(void)
 
 	if(! svec.empty()) // ... ok, call my_find()
 	{
+		cout << "svec is not empty!" << endl;
 		cout << "ok, call my_find()" << endl;
-		cout << my_find(&vec[0], &vec[vec.size()], search_value) << endl;
+
+		// A vector also holds its elements in a contiguous area of memory,
+		// so we can pass my_find() a begin/end pair of addresses in the same way
+		// as we do for the built-in array, except in one case.
+		// Unlike an array, a vector can be empty.
+		cout << *my_find(&svec[0], &svec[svec.size()], search_value) << endl;
 	}
 	else
 		cout << "an empty vector" << endl;
+
+	search_value = "the third";
+	cout << *my_find( my_begin(svec), my_end(svec), search_value) << endl;
+	// find() begin() end() all have same keyname in lib header file!!!
 
 	return 0;
 }
