5afe461c1502089952d1b3253370e9f88d9d0c0b
qwer <Davego@protonmail.com>
2022-10-08 23:40:57 +0800

    constructor overload

--- code/Essential C++ by Stanley B.Lippman/Chapter 4. Object-Based Programming/4.2/4.2.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 4. Object-Based Programming/4.2/4.2.cpp
@@ -4,6 +4,41 @@
 
 using std::cout;
 
+class Triangular {
+public:
+	// overloaded set of constructors
+	Triangular();	// default constructor
+	Triangular(int len);
+	Triangular(int len = 1, int bp = 1);
+	Triangular(const Triangular&);
+
+private:
+	int _length;	// number of elements
+	int _beg_pos;	// beginning position of range
+	int _next;		// next element to iterate over
+};
+
+Triangular::Triangular()
+{
+	_length = 1;
+	_beg_pos = 1;
+	_next = _beg_pos - 1;
+}
+
+Triangular::Triangular(int len, int bp)
+{
+	// _length and _beg_pos both must be at least 1
+	// best not to trust the user to always be right
+	_length = len > 0 ? len : 1;
+	_beg_pos = bp > 0 ? bp : 1;
+	_next = _beg_pos - 1;
+}
+
+Triangular::Triangular(const Triangular &rhs)
+	: _length (rhs._length),
+	  _beg_pos(rhs._beg_pos),_next(rhs._beg_pos-1)
+{} // yes, empty!
+
 int main()
 {
 	cout << "Hello, Constructors!\n";
