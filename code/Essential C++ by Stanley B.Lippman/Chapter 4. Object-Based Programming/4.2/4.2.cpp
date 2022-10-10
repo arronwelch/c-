6c3a826ca3eeb8a5d55a425cabe2b381fdfa1fc6
qwer <Davego@protonmail.com>
2022-10-10 23:27:12 +0800

    default constructor with default init-value

--- code/Essential C++ by Stanley B.Lippman/Chapter 4. Object-Based Programming/4.2/4.2.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 4. Object-Based Programming/4.2/4.2.cpp
@@ -9,9 +9,13 @@ using std::string;
 class Triangular {
 public:
 	// overloaded set of constructors
-	Triangular();	// default constructor
-	Triangular(int len);
+	//Triangular();	// default constructor
+	//Triangular(int len);
+	//Triangular(int len, int bp);
+
+	// also a default constructor
 	Triangular(int len = 1, int bp = 1);
+
 	Triangular(const Triangular&);
 
 private:
@@ -21,12 +25,22 @@ private:
 	int _next;		// next element to iterate over
 };
 
-Triangular::Triangular()
-{
-	_length = 1;
-	_beg_pos = 1;
-	_next = _beg_pos - 1;
-}
+// Triangular::Triangular()
+// {
+// 	_length = 1;
+// 	_beg_pos = 1;
+// 	_next = _beg_pos - 1;
+// }
+// 
+// Triangular::Triangular(int len)
+// 	: _name("Triangular")
+// {
+// 	// _length and _beg_pos both must be at least 1
+// 	// best not to trust the user to always be right
+// 	_length = len > 0 ? len : 1;
+// 	_beg_pos = 1;
+// 	_next = _beg_pos - 1;
+// }
 
 Triangular::Triangular(int len, int bp)
 	: _name("Triangular")
@@ -38,6 +52,7 @@ Triangular::Triangular(int len, int bp)
 	_next = _beg_pos - 1;
 }
 
+// The Member Initialization List
 Triangular::Triangular(const Triangular &rhs)
 	: _length (rhs._length),
 	  _beg_pos(rhs._beg_pos),_next(rhs._beg_pos-1)
@@ -45,6 +60,9 @@ Triangular::Triangular(const Triangular &rhs)
 
 int main()
 {
-	Triangular tri1(1,1);
+	Triangular tri1;
+	Triangular tri2(2);
+	Triangular tri3(3,3);
+	Triangular tri4(tri3);
 	cout << "Hello, Constructors!\n";
 }
