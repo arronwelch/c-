d7eb5919e0e0eaaac2c8a41286404ed0a580dd48
qwer <Davego@protonmail.com>
2022-10-25 23:57:24 +0800

    pointer to class member functions

---
+++ code/Essential C++ by Stanley B.Lippman/Chapter 4. Object-Based Programming/4.11/4.11.cpp
@@ -0,0 +1,19 @@
+// 4.11.cpp: Pointers to Class Member Functions
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	// num_sequence ns;
+	// const int pos = 8;
+	// for (int ix = 1; ix < num_sequence::num_of_sequences(); ++ix)
+	// {
+	// 	ns.set_sequence(num_sequence::nstype(ix));
+	// 	int elem_val = ns.elem(pos);
+	// 	display(cout, ns, pos, elem_val);
+	// }
+
+	cout << "Hello, Pointer to Class Member Functions!\n";
+}
