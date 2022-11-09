bf3d85b4430c3f372bc7233c872dad6a97831a26
nick <Davego@protonmail.com>
2022-11-09 23:58:45 +0800

    add gen_elems()

--- code/Essential C++ by Stanley B.Lippman/Chapter 5. Object-Oriented Programming/5.5.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 5. Object-Oriented Programming/5.5.cpp
@@ -41,7 +41,21 @@ ostream& Fibonacci::print(ostream &os) const
 
 void Fibonacci::gen_elems(int pos) const
 {
+	if (_elems.empty())
+		{ _elems.push_back(1); _elems.push_back(1); }
 
+	if (_elems.size() < pos)
+		{
+			int ix = _elems.size();
+			int n_2 = _elems[ix-2];
+			int n_1 = _elems[ix-1];
+			for(; ix < pos; ++ix)
+			{
+				int elem = n_2 + n_1;
+				_elems.push_back(elem);
+				n_2 = n_1; n_1 = elem;
+			}
+		}
 }
 
 int main()
