f3f2184c42ec13799428a8b755f5d7a759e0a3d3
nick <Davego@protonmail.com>
2022-11-10 23:53:31 +0800

    reuses the name of an inherited base class member

--- code/Essential C++ by Stanley B.Lippman/Chapter 5. Object-Oriented Programming/5.5.cpp
+++ code/Essential C++ by Stanley B.Lippman/Chapter 5. Object-Oriented Programming/5.5.cpp
@@ -14,6 +14,7 @@ public:
 	int length() const { return _length; }
 	int beg_pos() const { return _beg_pos; }
 protected:
+	bool check_integrity(int pos) const;
 	virtual void gen_elems(int pos) const;
 	int _length;
 	int _beg_pos;
@@ -25,6 +26,7 @@ vector<int> Fibonacci::_elems;
 int Fibonacci::
 elem(int pos) const
 {
+	// now resolves to Fibonacci's instance
 	if (! check_integrity(pos))
 		return 0;
 
@@ -34,28 +36,53 @@ elem(int pos) const
 	return _elems[pos-1];
 }
 
-ostream& Fibonacci::print(ostream &os) const
-{
-	return os;
-}
-
 void Fibonacci::gen_elems(int pos) const
 {
 	if (_elems.empty())
 		{ _elems.push_back(1); _elems.push_back(1); }
 
 	if (_elems.size() < pos)
+	{
+		int ix = _elems.size();
+		int n_2 = _elems[ix-2];
+		int n_1 = _elems[ix-1];
+
+		for(; ix < pos; ++ix)
 		{
-			int ix = _elems.size();
-			int n_2 = _elems[ix-2];
-			int n_1 = _elems[ix-1];
-			for(; ix < pos; ++ix)
-			{
-				int elem = n_2 + n_1;
-				_elems.push_back(elem);
-				n_2 = n_1; n_1 = elem;
-			}
+			int elem = n_2 + n_1;
+			_elems.push_back(elem);
+			n_2 = n_1; n_1 = elem;
 		}
+	}
+}
+
+ostream& Fibonacci::
+print(ostream &os) const
+{
+	int elem_pos = _beg_pos-1;
+	int end_pos = elem_pos + _length;
+
+	if (end_pos > _elems.size())
+		Fibonacci::gen_elems(end_pos);
+
+	while (elem_pos < end_pos)
+		os << _elems[elem_pos++] << ' ';
+
+	return os;
+}
+
+inline bool Fibonacci::
+check_integrity(int pos) const
+{
+	// class scope operator necessary ...
+	// unqualified name resolves to this instance!
+	if (! num_sequence::check_integrity(pos))
+		return false;
+
+	if (pos > _elems.size())
+		Fibonacci::gen_elems(pos);
+
+	return true;
 }
 
 int main()
@@ -63,12 +90,22 @@ int main()
 	cout << "Hello, a Derived Class!\n";
 
 	num_sequence *ps = new Fibonacci(12, 8);
+	cout << "new Fibonacci(12, 8)\n";
+
+	cout << "Fibonacci(8) = " << ps->elem(8) << endl;
 
 	// ok: invokes Fibonacci::what_am_i() through virtual mechanism
-	ps->what_am_i();
+	cout << "ps->what_am_i() : "
+		<< ps->what_am_i()
+		<< endl;
 
 	// ok: invokes inherited num_sequence::max_elems();
-	ps->max_elems();
+	cout << "ps->max_elems() : "
+		<< ps->max_elems()
+		<< endl;
+
+	cout << "display *ps:\n";
+	cout << *ps << endl;
 
 	// error: length() is not part of num_sequence interface
 	// ps->length();
