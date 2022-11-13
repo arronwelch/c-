324fc497ad06c3c19988a3add8f412e1eebb6033
nick <Davego@protonmail.com>
2022-11-13 23:58:00 +0800

    copy constructor

---
+++ code/Essential C++ by Stanley B.Lippman/Chapter 5. Object-Oriented Programming/5.8.cpp
@@ -0,0 +1,15 @@
+// 5.8.cpp: Initialization, Destruction, and Copy
+
+inline Fibonacci::
+Fibonacci(int len, int beg_pos)
+	: num_sequence(len, beg_pos, &_elems)
+{}
+
+num_sequence::
+num_sequence(int len = 1, int bp = 1, vector<int> *pe=0)
+	: _length(len), _beg_pos(bp), _pelems(pe) {}
+
+Fibonacci::Fibonacci(const Fibonacci &rhs)
+	: num_sequence(rhs)
+{}
+
